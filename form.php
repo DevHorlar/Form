86bde9e3507b51204bce2e431641ca28443440c2
QuantumBen <omosabenedict@gmail.com>
2025-05-18 10:34:15 -0700

    wip

--- form.php
+++ form.php
@@ -8,7 +8,16 @@ $email = $_POST['email']."\n";
 $address = $_POST['address']."\n";
 
 
+// echo "<h1> $name </h1>";
 
+// var_dump($name);
+
+
+
+if( isset($name) && !empty($name) )
+{
+    echo "Benedict the name is empty";
+}
 
 
 
