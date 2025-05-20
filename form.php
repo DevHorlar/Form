31e780fdc173ed112d2d0aac69ecb95b5105876f
QuantumBen <omosabenedict@gmail.com>
2025-05-20 13:25:37 -0700

    wip

--- form.php
+++ form.php
@@ -1,8 +1,23 @@
 <?php
 
+$to = 'opeyemibusuyi@gmail.com';
+$subject = "contact us";
+
 $name = isset($_POST['name']) ? trim($_POST['name']) : '';
 $email = isset($_POST['email']) ? trim($_POST['email']) : '';
 $address = isset($_POST['address']) ? trim($_POST['address']) : '';
+
+$message = "'$name' . '$email' . '$address'";
+$additional_headers = "additional headers";
+$mail = mail($to, $subject, $message, $additional_headers);
+
+if($mail)
+{
+    echo "Message sent";
+}
+
+
+
 
 if (empty($name)) {
     echo "name is empty";
