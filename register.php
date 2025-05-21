44d741eec7ad964a230a4450900c3cdd6198e4ef
DevHorlar <opeyemibusuyi@gmail.com>
2025-05-21 22:25:23 +0100

    wip

---
+++ register.php
@@ -0,0 +1,21 @@
+<?php
+$to = 'opeyemibusuyi@gmail.com';
+$subject = "contact us";
+
+$name = isset($_POST['name']) ? trim($_POST['name']) : '';
+$email = isset($_POST['email']) ? trim($_POST['email']) : '';
+$address = isset($_POST['address']) ? trim($_POST['address']) : '';
+
+$message = "'$name' . '$email' . '$address'";
+$additional_headers = "additional headers";
+$mail = mail($to, $subject, $message, $additional_headers);
+
+$new = "$subject . $message . $additional_headers";
+
+ $fp = fopen("ope.txt","a");
+
+    fputs($fp,$new);
+
+    fclose($fp);
+
+?>
